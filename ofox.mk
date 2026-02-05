102d5b42c1959e5ffbf155e72ad85fff4c9c60a8
MengWanYu <mengwanyu@example.com>
2026-02-05 09:54:11 +0800

    Fix: Remove OrangeFox common dependency to build independently

--- ofox.mk
+++ ofox.mk
@@ -4,8 +4,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from OrangeFox common configuration
-$(call inherit-product, vendor/orangefox/config/common.mk)
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # A/B partition support
 AB_OTA_UPDATER := true
